845324ef25910cd90a9dcc6266ed158ea51e4b39
Daniel Vaughn <danielbvaughn@gmail.com>
2024-08-03 17:35:28 -0400

    adds node adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,15 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import yaml from "@rollup/plugin-yaml";
 
-import netlify from "@astrojs/netlify";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: "server",
-  adapter: netlify(),
+  adapter: node({
+    mode: "standalone"
+  }),
   security: {
     checkOrigin: true
   },
